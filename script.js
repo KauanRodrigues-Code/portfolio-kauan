e43823318b1851dc6e3182d796b2d3b9eb9ec86a
KauanRodrigues-Code <kauancorinthiano@icloud.com>
2026-02-12 13:32:51 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 gsap.registerPlugin(ScrollTrigger);
 
+// Reveal padrão
 gsap.utils.toArray(".reveal").forEach((el) => {
   gsap.to(el, {
     scrollTrigger: {
       trigger: el,
-      start: "top 80%",
-      toggleActions: "play none none none"
+      start: "top 85%",
     },
     y: 0,
     opacity: 1,
     duration: 1.2,
     ease: "power3.out"
+  });
+});
+
+// Parallax + zoom + rotação nas imagens
+gsap.utils.toArray(".watch").forEach((img) => {
+  gsap.to(img, {
+    scrollTrigger: {
+      trigger: img,
+      start: "top bottom",
+      end: "bottom top",
+      scrub: true
+    },
+    y: -120,
+    scale: 1.1,
+    rotate: 5
   });
 });
