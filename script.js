ae55f1e996c62d8cfe9359b44d181d347be94128
KauanRodrigues-Code <kauancorinthiano@icloud.com>
2026-02-19 16:17:07 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const reveals = document.querySelectorAll(".reveal");
 
-window.addEventListener("scroll", () => {
-  reveals.forEach(section => {
-    const windowHeight = window.innerHeight;
-    const revealTop = section.getBoundingClientRect().top;
-    const revealPoint = 100;
-
-    if(revealTop < windowHeight - revealPoint){
-      section.classList.add("active");
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
     }
   });
+}, {
+  threshold: 0.15
+});
+
+reveals.forEach(section => {
+  observer.observe(section);
 });
